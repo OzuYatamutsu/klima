d88417fd0e85edbfe1fdcfdf3745c5546623526a
Sean Collins <secollins@linkedin.com>
2017-03-31 10:01:26 -0700

    Connect Influx and set up db; TODO: no exposure of client in setup??

---
+++ config.py
@@ -0,0 +1,14 @@
+## Logging
+## TODO
+
+## InfluxDB settings
+influx_settings = {
+    'username': 'root',
+    'password': 'root',
+    'hostname': 'localhost',
+    'port': 8086,
+    'database': '_klima_dev', 
+    'ssl': False,
+    'enabled': True
+}
+
---
+++ influx/influx_adapter.py
@@ -0,0 +1,40 @@
+from config import influx_settings
+from influxdb import InfluxDBClient
+from logging import getLogger, DEBUG
+logger = getLogger(__name__)
+logger.setLevel(DEBUG) # TODO
+
+# Access db object from outside adapter 
+global influx_client
+
+username = influx_settings['username']
+password = influx_settings['password']
+hostname = influx_settings['hostname']
+port = influx_settings['port']
+database = influx_settings['database']
+use_ssl = influx_settings['ssl']
+
+def initialize():
+    """
+    Connects the client to the database (and assigns a value to influx_client).
+    """
+    
+    influx_client = InfluxDBClient(
+        host = hostname,
+        port = port,
+        username = username,
+        password = password,
+        database = database,
+        ssl = use_ssl
+    )
+    logger.debug('Opening new connection to InfluxDB.')
+
+def get_client():
+    """
+    Returns the client object, initializing if necessary.
+    """
+
+    if influx_client is None:
+      initialize()
+    return influx_client
+
