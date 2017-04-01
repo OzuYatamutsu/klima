3cc40f00f4f01ea921123d2bcbb4c71b5735d35a
Sean Collins <secollins@linkedin.com>
2017-03-31 19:18:02 -0700

    Config of stream type

--- config.py
+++ config.py
@@ -1,11 +1,24 @@
 from logging import basicConfig, DEBUG, INFO, WARNING, ERROR, CRITICAL
+from sensors.sensor_type import SensorType
 
-# Sensor data location - IMPORTANT!
-# Replace value below with the /dev location of your serial devices.
-# It is expected that reading these sensors will return a single numerical value:
+'''
+Sensor data location - IMPORTANT!
+Replace value below with the /dev location of your data streams.
+
+It is expected that reading these location will return a single numerical value.
+
+Valid values for type are SensorType.FILE (if locations point to a file) 
+or SensorType.SERIAL (if they point to serial interfaces)
+'''
+
 sensors = {
     'temperature': '~/dev/test_0',
-    'humidity': '~/dev/test_1'
+    'humidity': '~/dev/test_1',
+    'type': SensorType.FILE,
+
+    # Ignore these if type is not set to SERIAL
+    'baud': 9600,
+    'timeout': 1
 }
 
 # Logging
@@ -21,4 +34,3 @@ influx_settings = {
     'ssl': False,
     'enabled': True
 }
-
---
+++ sensors/sensor_type.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class SensorType(Enum):
+    SERIAL = 0,
+    FILE = 1
