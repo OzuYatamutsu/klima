bd45609bdfbefb143d47bfc14af380608f1d96b2
Sean Collins <secollins@linkedin.com>
2017-04-05 21:06:35 -0700

    Finish test route implementation

--- test/test_routes.py
+++ test/test_routes.py
@@ -27,49 +27,91 @@ class TestRoutes(TestCase):
         Tests if /api/<string:sensor_type> returns both a 200 and some values
         """
 
-        pass  # TODO
+        response = self.app.get('/api/temperature')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'0.0')
+
+        response = self.app.get('/api/humidity')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'0.0')
 
     def test_get_temp_or_humidity_at_time(self):
         """
         Tests if /api/<string:sensor_type>/<string:timescale> returns both a 200 and some values
         """
 
-        pass  # TODO
+        response = self.app.get('/api/temperature/5m')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'{}')
+
+        response = self.app.get('/api/humidity/5m')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'{}')
 
     def test_get_location_temp_or_humidity(self):
         """
         Tests if /api/<string:sensor_type>/location returns both a 200 and some values
         """
 
-        pass  # TODO
+        response = self.app.get('/api/temperature/location')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'0.0')
+
+        response = self.app.get('/api/humidity/location')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'0.0')
 
     def test_get_location_temp_or_humidity_at_time(self):
         """
         Tests if /api/<string:sensor_type>/location/<string:timescale> returns both a 200 and some values
         """
 
-        pass  # TODO
+        response = self.app.get('/api/temperature/location/5m')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'{}')
+
+        response = self.app.get('/api/humidity/location/5m')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'{}')
 
     def test_get_location_temp_or_humidity_diff(self):
         """
         Tests if /api/<string:sensor_type>/location/diff returns both a 200 and some values
         """
 
-        pass  # TODO
+        response = self.app.get('/api/temperature/location/diff')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'0.0')
+
+        response = self.app.get('/api/humidity/location/diff')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'0.0')
 
     def test_get_location_temp_or_humidity_diff_at_time(self):
         """
         Tests if /api/<string:sensor_type>/location/diff/<string:timescale> returns both a 200 and some values
         """
 
-        pass  # TODO
+        response = self.app.get('/api/temperature/location/diff/5m')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'{}')
+
+        response = self.app.get('/api/humidity/location/diff/5m')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'{}')
 
     def test_get_location_temp_or_humidity_diff_avg_at_time(self):
         """
         Tests if /api/<string:sensor_type>/location/diff/avg/<string:timescale> returns both a 200 and some values
         """
 
-        pass  # TODO
+        response = self.app.get('/api/temperature/location/diff/avg/5m')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'{}')
+
+        response = self.app.get('/api/humidity/location/diff/avg/5m')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data, b'{}')
 
 
 if __name__ == '__main__':
